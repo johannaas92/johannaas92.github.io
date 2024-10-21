b998eef22ff4d8bd8b2c6e2b8a2b3478cfe4f67e
johannaas92 <fridman.johanna@gmail.com>
2024-10-21 19:27:55 +0200

    add js

---
+++ script.js
@@ -0,0 +1,6 @@
+//Created a welcome function that welcomes everybody.
+
+var firstname = prompt('Vad heter du i förnamn?'); 
+var lastname = prompt('Vad heter du i efternamn?'); 
+alert ('Välkommen'+' '+firstname+' '+lastname+'!');
+
